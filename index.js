1106015f6b9dda22ba15d25543cdc99128be16f2
Thomas Hallock <hallock@gmail.com>
2016-09-07 14:01:26 -0700

    not edpending on es6 syntax

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 const React = require('react');
-class DOMElementWrapper extends React.Component {
-    render() {
+const DOMElementWrapper = React.createClass({
+    render: function () {
         return React.DOM.div({
             className: 'inner', ref: el => this.el = el
         });
     }
-}
+});
 DOMElementWrapper.prototype.componentDidMount =
 DOMElementWrapper.prototype.componentDidUpdate = function () {
     this.el.innerHTML = '';
